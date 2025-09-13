f06d9e2a4b6eb265bbf6e973f16a9b8fcacab995
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-09-13 20:33:18 +0200

    Move simpleuserprovider to template

---
+++ app/Auth/SimpleUserProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth;
+
+use App\Models\User;
+use BaseApi\Auth\UserProvider;
+use Exception;
+
+/**
+ * Default UserProvider implementation using database or stub fallback.
+ */
+class SimpleUserProvider implements UserProvider
+{
+    /**
+     * Resolve user by ID from database or return stub.
+     */
+    public function byId(string $id): ?array
+    {
+        try {
+            return User::firstWhere('id', '=', $id)?->jsonSerialize();
+        } catch (Exception) {
+            return null;
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,10 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Auth\SimpleUserProvider;
 use BaseApi\App;
 
 // Boot the application
 App::boot(__DIR__ . '/..');
+App::setUserProvider(new SimpleUserProvider());
 
 // Load routes
 require_once __DIR__ . '/../routes/api.php';
