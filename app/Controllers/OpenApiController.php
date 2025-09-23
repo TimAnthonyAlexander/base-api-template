6b76f79f21c6f4dcaf0ed80459c4e4834ce79fc6
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-09-23 20:03:59 +0200

    Update baseapi

--- app/Controllers/OpenApiController.php
+++ app/Controllers/OpenApiController.php
@@ -8,7 +8,6 @@ use BaseApi\Controllers\Controller;
 use BaseApi\Http\JsonResponse;
 use BaseApi\Http\Response;
 use BaseApi\OpenApi\OpenApiGenerator;
-use BaseApi\OpenApi\OpenApiCache;
 use Exception;
 
 final class OpenApiController extends Controller
@@ -16,20 +15,10 @@ final class OpenApiController extends Controller
     public function get(): Response
     {
         try {
-            $cache = new OpenApiCache();
-            
-            // Try to get cached OpenAPI spec
-            $spec = $cache->get();
-            
-            if ($spec === null) {
-                // Generate fresh OpenAPI spec
-                $generator = new OpenApiGenerator();
-                $spec = $generator->generate();
-                
-                // Cache the generated spec
-                $cache->put($spec);
-            }
-            
+            // Generate fresh OpenAPI spec
+            $generator = new OpenApiGenerator();
+            $spec = $generator->generate();
+
             return new JsonResponse($spec);
         } catch (Exception $e) {
             return JsonResponse::error('Failed to generate OpenAPI specification: ' . $e->getMessage());
